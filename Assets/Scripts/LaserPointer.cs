59bb40cd9dd3f2de2f4278cd21a6237a0065754e
DESKTOP-4RG4IVB\ruixi <ruixiong8@hotmail.com>
2023-03-05 16:47:49 +0800

    add teleport and throw objects

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LaserPointer : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean teleportAction;
+
+    public GameObject laserPrefab; 
+    private GameObject laser; 
+    private Transform laserTransform; 
+    private Vector3 hitPoint; 
+
+    // teleport variables
+    public Transform cameraRigTransform; 
+    public GameObject teleportReticlePrefab;
+    private GameObject reticle;
+    private Transform teleportReticleTransform; 
+    public Transform headTransform; 
+    public Vector3 teleportReticleOffset; 
+    public LayerMask teleportMask; 
+    private bool shouldTeleport; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+
+        // teleport
+        reticle = Instantiate(teleportReticlePrefab);
+        teleportReticleTransform = reticle.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (teleportAction.GetState(handType))
+        {
+            print("Teleporting action");
+            RaycastHit hit;
+            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100, teleportMask)) {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+
+                // set up teleport reticle
+                reticle.SetActive(true);
+                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                shouldTeleport = true;
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+
+            // teleport reticle
+            reticle.SetActive(false);
+        }
+
+        if (teleportAction.GetStateUp(handType) && shouldTeleport)
+        {
+            Teleport();
+        }
+    }
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(controllerPose.transform.position, hitPoint, .5f);
+        laserTransform.LookAt(hitPoint);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x,
+                                                laserTransform.localScale.y,
+                                                hit.distance);
+    }
+
+    // teleport method
+    private void Teleport()
+    {
+        shouldTeleport = false;
+        reticle.SetActive(false);
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+        difference.y = 0;
+        cameraRigTransform.position = hitPoint + difference;
+    }
+}
