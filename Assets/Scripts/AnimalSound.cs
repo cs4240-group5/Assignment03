80cd554fdd2ac38b836586e3201c772b84cea5cd
xiinweii98 <xiinweii98@gmail.com>
2023-03-10 04:29:11 +0800

    Update animal sounds

--- Assets/Scripts/AnimalSound.cs
+++ Assets/Scripts/AnimalSound.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class AnimalSound : MonoBehaviour
 {
+    private AudioSource source;
     public AudioClip animalSound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Random.value < .1)
+        if(Random.value < .01 && !source.isPlaying)
         {
-            GetComponent<AudioSource>().PlayOneShot(animalSound);
+            //source.PlayOneShot(animalSound);
+            source.clip = animalSound;
+            source.PlayDelayed(Random.Range(10f, 20f));
         }
     }
 }
